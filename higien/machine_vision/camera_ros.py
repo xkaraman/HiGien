a3ae9b1c2d53e5485cec1ca069f8e29d8780cfd3
Xenofon Karamanos <xenakiskaramanos@gmail.com>
2022-12-12 13:17:33 +0200

    Update human_detection module

--- higien/machine_vision/camera_ros.py
+++ higien/machine_vision/camera_ros.py
@@ -1,128 +1,266 @@
-import rclpy
-from rclpy.node import Node
-from sensor_msgs.msg import Image, CameraInfo
-from cv_bridge import CvBridge
-import message_filters
-import cv2 as cv
-import pyrealsense2.pyrealsense2 as rs
-import numpy as np
+# SYSTEM
 import time
 import sys
-from yolov5 import *
+
+#
+import numpy as np
+import cv2 as cv
+import pyrealsense2.pyrealsense2 as rs
+
+# ROS
+import rclpy
+import tf2_ros
+import tf2_geometry_msgs
+import message_filters
+
+from rclpy.node import Node
+from sensor_msgs.msg import Image, CompressedImage, CameraInfo
 
 from std_srvs.srv import Trigger
+from visualization_msgs.msg import MarkerArray, Marker
+from geometry_msgs.msg import Point, PointStamped
+
+from cv_bridge import CvBridge
+
+# LOCAL
+from .yolov5 import Yolov5
+from .utilities import save_csv, line_count
 
 
 class Camera(Node):
-    def __init__(self):
-        super().__init__("camera_node")  # type: ignore
-        # ros2 topics
-        aligned_depth_topic = "/camera/aligned_depth_to_color/image_raw"
-        bgr_topic = "/camera/color/image_raw"
-        aligned_info_topic = "/camera/aligned_depth_to_color/camera_info"
 
-        # yolov5 model
-        self.model = Yolov5()
+  def __init__(self):
+    super().__init__("human_detection_node")  # type: ignore
+    # ros2 topics
+    self.aligned_depth_topic = "/camera/aligned_depth_to_color/image_raw"
+    self.bgr_topic = "/camera/color/image_raw"
+    self.aligned_info_topic = "/camera/aligned_depth_to_color/camera_info"
 
-        self.enabled = False
-        self.enable_service = self.create_service(Trigger, 'higien/enable', self.enable)
-        self.disable_service = self.create_service(Trigger, 'higien/disable', self.disable)
-        
-        self.humans_found = {'points': []}
-        self.cycle_time = 3600 # seconds
-        self.cycle_time_start_time = 0
+    # yolov5 model
+    self.model = Yolov5()
 
-        # Setting ros2 subscribers and callback to receive multiple topic streams
-        self.bridge = CvBridge()
-        self.depth_sub = message_filters.Subscriber(self, Image, aligned_depth_topic)
-        self.image_sub = message_filters.Subscriber(self, Image, bgr_topic)
-        self.info_sub = message_filters.Subscriber(self, CameraInfo, aligned_info_topic)
-        self.time_synchronizer = message_filters.ApproximateTimeSynchronizer([self.depth_sub, self.image_sub, self.info_sub], queue_size=1, slop=0.1)
-        self.time_synchronizer.registerCallback(self.callback)
-        self.get_logger().info('Starting {}'.format(self.get_name()))
+    self.enabled = False
+    self.enable_service = self.create_service(Trigger,
+                                              'higien/detection/enable',
+                                              self.enable)
+    self.disable_service = self.create_service(Trigger,
+                                               'higien/detection/disable',
+                                               self.disable)
 
-        self.detection_pub = self.create_publisher(Image, "higien/detection", 10)
+    self.get_detections_points = self.create_service(Trigger,
+                                                     'higien/get_detections',
+                                                     self.getDetections)
+    self.humans_points = {'points': []}
+    self.humans_points_marker = Marker()
 
+    self.cycle_time = 3600.0  # seconds
+    self.cycle_time_start_time = 0.0
 
-    def enable(self,request, response):
-        self.get_logger().info('Incoming request to enable human_detection')
-        # Prepare response
-        response.success = True
-        if self.enabled:
-            response.message = "Higien/human_detection already enabled"
-            return response
-        
-        self.enabled = True
-        response.message = "Higien/human_detection enabled"
-        self.get_logger().info('Higien/human_detection enabled')
+    # Setting ros2 subscribers and callback to receive multiple topic streams
+    self.bridge = CvBridge()
+    # self.depth_sub = message_filters.Subscriber(self, Image, self.aligned_depth_topic)
+    # self.image_sub = message_filters.Subscriber(self, Image, self.bgr_topic)
 
-        return response
+    self.depth_sub = message_filters.Subscriber(self, Image,
+                                                self.aligned_depth_topic)
+    self.image_sub = message_filters.Subscriber(self, Image, self.bgr_topic)
 
-    def disable(self,request, response):
-        self.get_logger().info('Incoming request to disable human_detection')
-        # Prepare response
-        response.success = True
-        if not self.enabled:
-            response.message = "Higien/human_detection already disabled"
-            return response
-        
-        self.enabled = False
-        response.message = "Higien/human_detection disabled"
-        self.get_logger().info('Higien/human_detection disabled')
+    self.info_sub = message_filters.Subscriber(self, CameraInfo,
+                                               self.aligned_info_topic)
 
-        return response
+    self.subs = [self.depth_sub, self.image_sub, self.info_sub]
+    self.time_synchronizer = message_filters.ApproximateTimeSynchronizer(
+        self.subs, queue_size=10, slop=0.1)
+    self.time_synchronizer.registerCallback(self.callback)
 
-    def callback(self, depth, image, info):
-        # Conversion os image and depth streams to opencv format
-        # self.get_logger().info('Got frame')
+    self.detection_pub = self.create_publisher(
+        Image, "higien/human_detection/image_raw", 10)
 
-        image_frame = self.bridge.imgmsg_to_cv2(image, desired_encoding="bgr8")
-        depth_frame = self.bridge.imgmsg_to_cv2(depth)
+    self.human_point_timer = self.create_timer(5, self.publish_points)
+    self.human_point_pub = self.create_publisher(
+        Marker, "higien/human_detection/points", 1)
 
-        # Setting camera intrinsics
-        start = time.perf_counter()
-        intrinsics = rs.intrinsics()
-        intrinsics.width = info.width
-        intrinsics.height = info.height
-        intrinsics.ppx = info.k[2]
-        intrinsics.ppy = info.k[5]
-        intrinsics.fx = info.k[0]
-        intrinsics.fy = info.k[4]
-        intrinsics.model = rs.distortion.none
-        intrinsics.coeffs = [i for i in info.d]
+    # ROS2 frame transformation
 
-        # Extracting bounding box coordinates, predicted object classes and object centers from image frame
-        self.model.detect_object_info(image_frame)
-        # Get xyz coordinates
-        coords = self.model.get_xyz_coordinates(intrinsics, depth_frame)
-        self.model.save_csv(coords)
-        # Drawing bounding boxes, class label and object distance
-        image_frame = self.model.draw_object_info(image_frame, depth_frame)
-        end = time.perf_counter()
-        fps = 1 / np.round(end - start, 3)
-        # print(f"FPS: {fps}")
+    self.buffer = tf2_ros.Buffer()
+    self.listener = tf2_ros.TransformListener(self.buffer, self)
 
-        # cv.putText(image_frame, f"FPS: {fps}", (20, 70), cv.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 255), 2)
-        
-        image_frame = self.bridge.cv2_to_imgmsg(image_frame)
+    self.camera_file = "/ros2_workspaces/higien_ws/src/higien/camera_frame.csv"
+    self.map_file = "/ros2_workspaces/higien_ws/src/higien/map_frame.csv"
 
-        self.detection_pub.publish(image_frame)
-        # self.get_logger().info('Publishing: detection')
+    self.initial_point = PointStamped()
+    self.transformed_point = 0
+    self.line_number = line_count(self.camera_file)
 
-        # cv.imshow("Image", image_frame)
-        # key = cv.waitKey(1)
-        # if key & 0xFF == ord('q'):
-        #     sys.exit()
+    self.get_logger().info(
+        'Starting {}. Please enable detection via service'.format(
+            self.get_name()))
+
+  def enable(self, request, response):
+    self.get_logger().info('Incoming request to enable human_detection')
+    # Prepare response
+    response.success = True
+    if self.enabled:
+      response.message = "Higien/human_detection already enabled"
+      return response
+
+    # Suscribe to topics
+    self.depth_sub = message_filters.Subscriber(self, Image,
+                                                self.aligned_depth_topic)
+    self.image_sub = message_filters.Subscriber(self, Image, self.bgr_topic)
+    self.info_sub = message_filters.Subscriber(self, CameraInfo,
+                                               self.aligned_info_topic)
+
+    self.subs = [self.depth_sub, self.image_sub, self.info_sub]
+    self.time_synchronizer = message_filters.ApproximateTimeSynchronizer(
+        self.subs, queue_size=1, slop=0.1)
+    self.time_synchronizer.registerCallback(self.callback)
+
+    self.enabled = True
+    response.message = "Higien/human_detection enabled"
+    self.get_logger().info('Higien/human_detection enabled')
+
+    return response
+
+  def disable(self, request, response):
+    self.get_logger().info('Incoming request to disable human_detection')
+    # Prepare response
+    response.success = True
+    if not self.enabled:
+      response.message = "Higien/human_detection already disabled"
+      return response
+
+    self.enabled = False
+    # Unsuscribe from topics
+    for sub in self.subs:
+      self.destroy_subscription(sub.sub)
+
+    response.message = "Higien/human_detection disabled"
+    self.get_logger().info('Higien/human_detection disabled')
+
+    return response
+
+  def getDetections(self, request, response):
+    # Save all points to file with appened date
+    self.get_logger().info('Incoming request to get human_detection points')
+    # Prepare response
+    response.success = True
+    # Save points to file
+    # Get current time from ROS2 formatted as string
+    current_time = self.get_clock().now().to_msg()
+    current_time = str(current_time)
+    current_time = current_time.replace(" ", "_")
+    saveFile = self.camera_file.replace(".csv", "_{}.csv".format(current_time))
+    save_csv(points, "", saveFile)
+
+    # Clear list only if Durateion of cycle has passed  (1 hour)  (3600
+    # seconds)
+    if (time.time() - self.cycle_time_start_time) > self.cycle_time:
+      self.humans_points['points'] = []
+      self.cycle_time_start_time = time.time()
+
+    response.message = "Higien/human_detection points saved to {}".format(
+        saveFile)
+    self.get_logger().info(
+        'Higien/human_detection points saved to {}'.format(saveFile))
+
+  def transform_coords(self, coords):
+    # print(coords)
+    try:
+      x, y, _ = coords
+      self.initial_point.point.x = x
+      self.initial_point.point.y = y
+      self.initial_point.point.z = 0.0
+      trans = self.buffer.lookup_transform('map', 'camera_link',
+                                           rclpy.time.Time())
+      self.transformed_point = tf2_geometry_msgs.do_transform_point(
+          self.initial_point, trans)
+      points = [self.transformed_point.point.x, self.transformed_point.point.y]
+      save_csv(points, self.map_file, self.camera_file)
+      self.get_logger().info('{}: Transform: {}'.format(
+          self.initial_point, self.transformed_point))
+
+      return self.transformed_point
+
+    except Exception as e:
+      self.get_logger().info(f'Cannot transform point {e}')
+
+  def callback(self, depth, image, info):
+    # Conversion os image and depth streams to opencv format
+    # self.get_logger().info('Got frame')
+
+    image_frame = self.bridge.imgmsg_to_cv2(image, desired_encoding="bgr8")
+    depth_frame = self.bridge.imgmsg_to_cv2(depth)
+    # image_frame = self.bridge.compressed_imgmsg_to_cv2(
+    #     image, desired_encoding="bgr8")
+    # depth_frame = self.bridge.compressed_imgmsg_to_cv2(depth)
+
+    # Setting camera intrinsics
+    start = time.perf_counter()
+    intrinsics = rs.intrinsics()
+    intrinsics.width = info.width
+    intrinsics.height = info.height
+    intrinsics.ppx = info.k[2]
+    intrinsics.ppy = info.k[5]
+    intrinsics.fx = info.k[0]
+    intrinsics.fy = info.k[4]
+    intrinsics.model = rs.distortion.none
+    intrinsics.coeffs = [i for i in info.d]
+
+    # Extracting bounding box coordinates, predicted object classes and object centers from image frame
+    self.model.detect_object_info(image_frame)
+    # Get xyz coordinates
+    coords = self.model.get_xyz_coordinates(intrinsics, depth_frame)
+    if coords is not None:
+      x, y, z = coords
+      point = Point()
+      point.x = x
+      point.y = y
+      point.z = z
+      transformedToMap = self.transform_coords(coords)
+      self.humans_points['points'].append(transformedToMap.point)
+
+    self.model.save_csv(coords)
+    # Drawing bounding boxes, class label and object distance
+    image_frame = self.model.draw_object_info(image_frame, depth_frame)
+    end = time.perf_counter()
+    fps = 1 / np.round(end - start, 3)
+    # cv.putText(image_frame, f"FPS: {fps}", (20, 70), cv.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 255), 2)
+
+    # Publish image with bounding boxes
+    image_frame = self.bridge.cv2_to_imgmsg(image_frame)
+    self.detection_pub.publish(image_frame)
+
+  def publish_points(self):
+    # Publish human points
+    marker = Marker()
+    marker.header.stamp = self.get_clock().now().to_msg()
+    marker.header.frame_id = "map"
+    marker.frame_locked = True
+    marker.ns = "human_points"
+    marker.id = 0
+    marker.type = Marker.POINTS
+    marker.action = Marker.ADD
+    marker.pose.orientation.w = 1.0
+    marker.scale.x = 0.1
+    marker.scale.y = 0.1
+    marker.color.a = 1.0
+    marker.color.g = 1.0
+
+    marker.points = self.humans_points['points']
+    self.humans_points_marker = marker
+    self.human_point_pub.publish(self.humans_points_marker)
+    # self.get_logger().info('Publishing: detection')
 
 
 def main(args=None):
-    # Initiallizing camera node
-    rclpy.init(args=args)
-    camera = Camera()
-    rclpy.spin(camera)
-    camera.destroy_node()
-    rclpy.shutdown()
+  # Initiallizing camera node
+  rclpy.init(args=args)
+  camera = Camera()
+  rclpy.spin(camera)
+  camera.destroy_node()
+  rclpy.shutdown()
 
 
 if __name__ == "__main__":
-    main()
+  main()
--- higien/machine_vision/yolov5.py
+++ higien/machine_vision/yolov5.py
@@ -10,136 +10,138 @@ import os
 
 
 class Yolov5():
-    def __init__(self):
-        # Loading pretrained model
-        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5n6')
-        self.classes = self.model.names
-        self.camera_file = "/ros2_workspaces/higien_ws/src/higien/camera_frame.csv"
-        if torch.cuda.is_available():
-            self.device = 'cuda'
-        else: 
-            self.device = 'cpu'
-            print('Cuda not available!')
 
-        # self.mot_tracker = Sort()
+  def __init__(self):
+    # Loading pretrained model
+    self.model = torch.hub.load('ultralytics/yolov5', 'yolov5n6')
 
-        # Setting detection threshold
-        self.detection_threshold = 0.7
+    self.classes = self.model.names
+    self.camera_file = "/ros2_workspaces/higien_ws/src/higien/camera_frame.csv"
+    if torch.cuda.is_available():
+      self.device = 'cuda'
+    else:
+      self.device = 'cpu'
+      print('Cuda not available!')
 
-        # Intiallizing lists for containing bounding box coordinates, class labels, object centers and object distance
-        self.obj_boxes = []
-        self.obj_ids = []
-        self.obj_classes = []
-        self.obj_centers = []
-        self.obj_confs = []
-        self.track_bbs_ids = 0
-        
-    def class_to_label(self, id):
-        return self.classes[int(id)]
+    # self.mot_tracker = Sort()
 
-    def detect_object_info(self, bgr_frame):
-        self.model.to(self.device)
-        results = self.model(bgr_frame)
-        # self.track_bbs_ids = self.mot_tracker.update(results.pred[0].cpu().numpy())
-        # Extracting object class and coordinates
-        labels, coordinates = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+    # Setting detection threshold
+    self.detection_threshold = 0.7
 
-        n = len(labels)
-        x_shape, y_shape = bgr_frame.shape[1], bgr_frame.shape[0]
+    # Intiallizing lists for containing bounding box coordinates, class labels, object centers and object distance
+    self.obj_boxes = []
+    self.obj_ids = []
+    self.obj_classes = []
+    self.obj_centers = []
+    self.obj_confs = []
+    self.track_bbs_ids = 0
 
-        self.obj_boxes = []
-        self.obj_classes = []
-        self.obj_centers = []
-        self.obj_confs = []
-        self.obj_ids = []
+  def class_to_label(self, id):
+    return self.classes[int(id)]
 
-        for i in range(n):
-            box = coordinates[i]
-            if box[4] > self.detection_threshold:
-                x1 = int(box[0] * x_shape)
-                y1 = int(box[1] * y_shape)
-                x2 = int(box[2] * x_shape)
-                y2 = int(box[3] * y_shape)
-                self.obj_boxes.append([x1, y1, x2, y2])
+  def detect_object_info(self, bgr_frame):
+    self.model.to(self.device)
+    results = self.model(bgr_frame)
+    # self.track_bbs_ids = self.mot_tracker.update(results.pred[0].cpu().numpy())
+    # Extracting object class and coordinates
+    labels, coordinates = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
 
-                cx = (x1 + x2) // 2
-                cy = (y1 + y2) // 2
+    n = len(labels)
+    x_shape, y_shape = bgr_frame.shape[1], bgr_frame.shape[0]
 
-                if cx >= 480:
-                    cx = 479
-                if cy >= 640:
-                    cy = 639
+    self.obj_boxes = []
+    self.obj_classes = []
+    self.obj_centers = []
+    self.obj_confs = []
+    self.obj_ids = []
 
-                self.obj_centers.append([cx, cy])
+    for i in range(n):
+      box = coordinates[i]
+      if box[4] > self.detection_threshold:
+        x1 = int(box[0] * x_shape)
+        y1 = int(box[1] * y_shape)
+        x2 = int(box[2] * x_shape)
+        y2 = int(box[3] * y_shape)
+        self.obj_boxes.append([x1, y1, x2, y2])
 
-                self.obj_classes.append(labels[i])
+        cx = (x1 + x2) // 2
+        cy = (y1 + y2) // 2
 
-                self.obj_confs.append(box[4].item())
-        
-        # for id in range(len(self.track_bbs_ids.tolist())):
-        #     coords = self.track_bbs_ids.tolist()[id]
-        #     x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
-        #     name_idx = int(coords[4])
+        if cx >= 480:
+          cx = 479
+        if cy >= 640:
+          cy = 639
 
-        #     self.obj_ids.append(name_idx)
-                
-    def get_xyz_coordinates(self, color_intrin, depth_frame):
-        for box in self.obj_centers:
-            cx, cy = box
-            depth = depth_frame[cx, cy]/1000
-            result = rs.rs2_deproject_pixel_to_point(color_intrin, [cx, cy], depth)
-            dx = result[2]
-            dy = - result[0]
-            dz = - result[1]
-            # dx = result[0]
-            # dy = result[1]
-            # dz = result[2]
-            print(f"X: {dx}, Y: {dy}, Z: {dz}")
-            
-            return dx, dy, dz
-    
-    def sort_id(self):
-        for obj_id in self.obj_ids:
-            return obj_id
+        self.obj_centers.append([cx, cy])
 
-    def draw_object_info(self, bgr_frame, depth_frame):
-        for box, class_id, obj_center in zip(self.obj_boxes, self.obj_classes, self.obj_centers):
-            # print("Class_id", class_id)
-            x1, y1, x2, y2 = box
-            cx, cy = obj_center
-            class_id = self.class_to_label(class_id)
+        self.obj_classes.append(labels[i])
 
-            depth = depth_frame[cx, cy]/1000
+        self.obj_confs.append(box[4].item())
 
-            
-            cv.line(bgr_frame, (cx, y1), (cx, y2), (0, 255, 0), 1)
-            cv.line(bgr_frame, (x1, cy), (x2, cy), (0, 255, 0), 1)
+    # for id in range(len(self.track_bbs_ids.tolist())):
+    #     coords = self.track_bbs_ids.tolist()[id]
+    #     x1, y1, x2, y2 = int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])
+    #     name_idx = int(coords[4])
 
-            cv.rectangle(bgr_frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
-            cv.putText(bgr_frame, "{} m".format(depth), (x1 + 5, y1 + 70), 0, 1.0, (125, 255, 0), 2)
-            # cv.putText(bgr_frame, f"ID: {obj_id}", (x1, y1 - 10), cv.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 125), 2)
-            cv.putText(bgr_frame, f"Object: {class_id}", (x1 + 60, y1 - 10), cv.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 125), 2)   
+    #     self.obj_ids.append(name_idx)
 
-        return bgr_frame
+  def get_xyz_coordinates(self, color_intrin, depth_frame):
+    for box in self.obj_centers:
+      cx, cy = box
+      depth = depth_frame[cx, cy] / 1000
+      result = rs.rs2_deproject_pixel_to_point(color_intrin, [cx, cy], depth)
+      dx = result[2]
+      dy = -result[0]
+      dz = -result[1]
+      # dx = result[0]
+      # dy = result[1]
+      # dz = result[2]
+      # print(f"X: {dx}, Y: {dy}, Z: {dz}")
 
-    def save_csv(self, coords):
-        headerList = ["X", "Y", "Z"]
-        filename = self.camera_file
-        file_exists = os.path.isfile(filename)
+      return dx, dy, dz
 
-        if coords == None:
-            dx, dy, dz = 0.0, 0.0, 0.0
-            with open(self.camera_file, 'a+') as f:
-                writer = csv.writer(f, delimiter=';')
-                if not file_exists:
-                    writer.writerow(headerList)
-                writer.writerow([dx, dy, dz])
-        else:
-            dx, dy, dz = coords
-            with open(self.camera_file, 'a+') as f:
-                writer = csv.writer(f, delimiter=';')
-                if not file_exists:
-                    writer.writerow(headerList)
-                writer.writerow([dx, dy, dz])
+  def sort_id(self):
+    for obj_id in self.obj_ids:
+      return obj_id
 
+  def draw_object_info(self, bgr_frame, depth_frame):
+    for box, class_id, obj_center in zip(self.obj_boxes, self.obj_classes,
+                                         self.obj_centers):
+      # print("Class_id", class_id)
+      x1, y1, x2, y2 = box
+      cx, cy = obj_center
+      class_id = self.class_to_label(class_id)
 
+      depth = depth_frame[cx, cy] / 1000
+
+      cv.line(bgr_frame, (cx, y1), (cx, y2), (0, 255, 0), 1)
+      cv.line(bgr_frame, (x1, cy), (x2, cy), (0, 255, 0), 1)
+
+      cv.rectangle(bgr_frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
+      cv.putText(bgr_frame, "{} m".format(depth), (x1 + 5, y1 + 70), 0, 1.0,
+                 (125, 255, 0), 2)
+      # cv.putText(bgr_frame, f"ID: {obj_id}", (x1, y1 - 10), cv.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 125), 2)
+      cv.putText(bgr_frame, f"Object: {class_id}", (x1 + 60, y1 - 10),
+                 cv.FONT_HERSHEY_PLAIN, 1.0, (0, 255, 125), 2)
+
+    return bgr_frame
+
+  def save_csv(self, coords):
+    headerList = ["X", "Y", "Z"]
+    filename = self.camera_file
+    file_exists = os.path.isfile(filename)
+
+    if coords == None:
+      dx, dy, dz = 0.0, 0.0, 0.0
+      with open(self.camera_file, 'a+') as f:
+        writer = csv.writer(f, delimiter=';')
+        if not file_exists:
+          writer.writerow(headerList)
+        writer.writerow([dx, dy, dz])
+    else:
+      dx, dy, dz = coords
+      with open(self.camera_file, 'a+') as f:
+        writer = csv.writer(f, delimiter=';')
+        if not file_exists:
+          writer.writerow(headerList)
+        writer.writerow([dx, dy, dz])
